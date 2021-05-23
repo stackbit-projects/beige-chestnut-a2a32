8051608406c1749d1a103d27c28ab3a8ea992f5a
Stackbit Code Editor <projects@stackbit.com>
2021-05-23 10:14:35 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../sass/main.scss';
-
+//a
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
